4ea396ff60e565ed5c3e51ec03cf1ed4df072d22
Handoyo <handoyoboruto@gmail.com>
2026-04-26 13:36:26 +0700

    Delete src directory

--- src/routes/mealPlannerRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { MealPlannerController } from "../controllers/mealPlannerController.js";
-
-const router = express.Router();
-router.get("/", MealPlannerController.getAll);
-router.post("/", MealPlannerController.create);
-
-export default router;
